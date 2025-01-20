052312ae2596368f1fba5cecb0a0203807e53f5b
Jagaa1122 <chjagaa@icloud.com>
2025-01-20 22:33:32 +0800

    cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import { useEffect, useState } from "react";
 type MovieType = {
   adult: boolean;
   backdrop_path: string;
-  genre_ids: [];
+  genre_ids: number[];
   id: number;
-  original_title: "";
-  original_language: "";
-  overview: "";
-  poster_path: "";
-  release_date: "";
-  title: "";
+  original_title: string;
+  original_language: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  title: string;
   video: boolean;
   popularity: number;
   vote_average: number;
@@ -20,10 +20,11 @@ type MovieType = {
 };
 
 export default function Home() {
-  const [movie, setMovie] = useState<MovieType | undefined>();
+  const [movies, setMovies] = useState<MovieType[]>([]);
   const token =
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwY2ZjN2Q0OWU5ZTE3MjMzZWFhZWY4YTU2ZmU3N2I4MSIsIm5iZiI6MTczNzM0MjQxOS45OTYsInN1YiI6IjY3OGRiZGQzZDhhNWIwZDAwYzQzNGNkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Yrklh2DdZ0DLG1mj3OzajlZUddJANak9_0ji5NQKvrQ";
-  const getMovie = async () => {
+
+  const getMovies = async () => {
     const response = await fetch(
       "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
       {
@@ -35,23 +36,33 @@ export default function Home() {
     );
     const data = await response.json();
     if (data?.results) {
-      setMovie(data.results[0]);
+      setMovies(data.results);
     }
   };
+
   useEffect(() => {
-    getMovie();
-    console.log("getting movie");
+    getMovies();
+    console.log("Getting movies...");
   }, []);
-  console.log({ movie });
+
   return (
-    <div className="flex flex-col gap-4 w-[375px] h-[510px] pl-[20px] pt-[20px]">
-      <div>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
-          alt=""
-        />
-      </div>
-      <div className="flex flex-col  gap-4">Hello: {movie?.original_title}</div>
+    <div className="flex flex-wrap">
+      {movies.map((movie) => (
+        <div
+          key={movie.id}
+          className="flex flex-col gap-4 w-[375px] h-[510px] pl-[20px] pt-[20px]"
+        >
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+              alt={movie.original_title}
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+             {movie.original_title}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
